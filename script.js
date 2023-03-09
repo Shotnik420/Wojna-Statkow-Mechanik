59ec17d2bab4aaa4cbe7154c89c7a35bb9c7b84d
SZOOTNIK* <62155481+Shotnik420@users.noreply.github.com>
2023-03-09 12:17:26 +0100

    update 9 marca 2023
    dodane grid i pola i chuj. Ból katusza cierpienie

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-var plansza = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+var plansza = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-for(i=0;i<10;i++){
-    for(j=1; j<11; j++){
-        document.getElementById("plansza").innerHTML += "<div id=" + plansza[i]+j +"'></div>"
-    }
+for (i = 0; i < 10; i++) {
+  for (j = 1; j < 11; j++) {
+    document.getElementById("plansza").innerHTML +=
+      "<div onmouseover='checker(this)' id='" + plansza[i] + j + "'></div>";
+  }
 }
+
+function checker(ele) {
+  let x = ele.id.charAt(0);
+  let y = ele.id.charAt(1);
+  console.log(x + " " + y);
+}
+
+function dodajStatek() {
+  document.querySelector("#siurek").style.opacity = 1;
+  console.log(pole);
+  document.getElementById(pole).style.backgroundColor = "gray";
+}
+
+document.addEventListener("mousemove", function (e) {
+  document.querySelector("#siurek").style.left = eval(e.clientX - 30) + "px";
+  document.querySelector("#siurek").style.top = eval(e.clientY - 30) + "px";
+});
